2d82b9502a04a6f34548f2e49dcd3e8aee915cf3
Andreas <aborge94@hotmail.com>
2017-04-26 23:28:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/projectsinglescreenapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.projectsinglescreenapp;
+
+import android.graphics.PixelFormat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+}
